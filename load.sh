1ed0ee0521b9df7d0dcefce9d4a6f2f05e8b6750
Demetry Pasсal <45700689+PasaOpasen@users.noreply.github.com>
2023-08-28 12:41:32 +0300

    -

--- load.sh
+++ load.sh
@@ -6,7 +6,7 @@ set -e
 #
 echo "import tar"
 
-file=/mnt/j/VirtualBoxes/common_folders/redos/redos73-base.tar
+file=/mnt/v/VirtualBoxes/common_folders/redos/redos73-base.tar
 
 if [ ! -f $file ]
 then
