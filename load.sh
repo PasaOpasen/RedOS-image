ab5cb73a23c257de4deabb4091f3daae6bea4e55
Demetry Pasсal <45700689+PasaOpasen@users.noreply.github.com>
2023-08-28 12:40:22 +0300

    -

--- load.sh
+++ load.sh
@@ -5,5 +5,14 @@ set -e
 # import tar from my paths as image
 #
 echo "import tar"
-cat /mnt/j/VirtualBoxes/common_folders/redos/redos73-base.tar | docker import - redos73-base 
+
+file=/mnt/j/VirtualBoxes/common_folders/redos/redos73-base.tar
+
+if [ ! -f $file ]
+then
+    echo "No such file $file"
+    exit 1
+fi
+
+cat $file | docker import - redos73-base 
 
