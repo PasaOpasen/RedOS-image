ed5957dbc3c5fe41f05f42496fa9dce9f41569e7
Demetry Pasсal <45700689+PasaOpasen@users.noreply.github.com>
2023-10-24 12:57:26 +0300

    -

---
+++ clean-locale.sh
@@ -0,0 +1,15 @@
+#
+# reduces locale-archive size 
+# https://unix.stackexchange.com/a/90016/532982
+#
+
+set -x -e
+
+localedef --list-archive | grep -v -i -E '^(en|ru)' | xargs localedef --delete-from-archive
+
+mv /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
+
+build-locale-archive
+
+yum reinstall glibc-common
+
