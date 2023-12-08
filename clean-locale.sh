0ad0812dba234afae0af1c42d317afb62ae10e93
Demetry Pasсal <45700689+PasaOpasen@users.noreply.github.com>
2023-12-08 13:04:52 +0300

    -

--- clean-locale.sh
+++ clean-locale.sh
@@ -5,11 +5,17 @@
 
 set -x -e
 
+echo -e 'After running this script u should manually call next command:\n\tdnf reinstall glibc-common -y'
+read -n 1 -p 'Press any key to continue...'
+
 localedef --list-archive | grep -v -i -E '^(en|ru)' | xargs localedef --delete-from-archive
 
 \mv /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
 
 build-locale-archive
 
+#
+# this command must be called directly cuz previous command performs logout
+#
 dnf reinstall glibc-common -y
 
